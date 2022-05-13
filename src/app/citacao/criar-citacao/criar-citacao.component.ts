dccd4ef5efbb19a137ba62269420de12fe36c20b
nayannelbatista <nayannelopes.bd@gmail.com>
2022-05-13 12:03:48 -0300

    Iniciando o projeto

---
+++ src/app/citacao/criar-citacao/criar-citacao.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-citacao',
+  templateUrl: './criar-citacao.component.html',
+  styleUrls: ['./criar-citacao.component.css']
+})
+export class CriarCitacaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/citacao/lista-citacao/lista-citacao.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-citacao',
+  templateUrl: './lista-citacao.component.html',
+  styleUrls: ['./lista-citacao.component.css']
+})
+export class ListaCitacaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
