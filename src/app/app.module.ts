75aa0c47b98e914aee9bacc85538ea732e596d5a
diegocgayoso <diegoc.gayoso@gmail.com>
2022-05-16 17:34:35 -0300

    Ajustando arquivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { CriarCitacaoComponent } from './componentes/citacao/criar-citacao/criar-citacao.component';
+import { ListaCitacaoComponent } from './componentes/citacao/lista-citacao/lista-citacao.component';
 import { RodapeComponent } from './componentes/rodape/rodape.component';
-import { ListaCitacaoComponent } from './citacao/lista-citacao/lista-citacao.component'
-import { CriarCitacaoComponent } from './citacao/criar-citacao/criar-citacao.component';
 
 @NgModule({
   declarations: [
